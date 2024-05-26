4c531fbd9f8327d6b9a5eac2d82b04886fc21ab8
Dan <daniel.d.cummings@gmail.com>
2024-05-25 19:23:17 -0700

    Add files via upload

--- novm.asm
+++ novm.asm
@@ -18,18 +18,18 @@ main:
 	inc eax ; eax=1 provides processor features and model related info.
 	CPUID	;*CPUID* provides processor details
 	bt ecx, 0x1f	;feature flag 0x1f(31) = 'CPUID_FEAT_ECX_HYPERVISOR' if true, then VM (yes)
-	je yesVm
+	jnc noVm ; cf = 1 if VM is detected
 
 	; this code gets executed if it's not inside a virtual machine (VM)
-	lea ecx, [falseMsg]
+	lea rcx, [trueMsg]
 	call printf
 	jmp end
 
-	yesVm:
-		lea ecx, [trueMsg]
+	noVm:
+		lea rcx, [falseMsg]
 		call printf
 		jmp end
 
 	end:
 		xor eax, eax
-		call ExitProcess
+		call ExitProcess 
