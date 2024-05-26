1a2d694a7908987ee29142b51e47637e2fd13b0f
Dan <daniel.d.cummings@gmail.com>
2024-05-25 17:32:52 -0700

    Add files via upload

--- novm.asm
+++ novm.asm
@@ -1,38 +1,35 @@
 bits 64
 default rel
+extern ExitProcess
+extern printf
 
 segment .data
 	; if not in VM, print "0"
-	noVm db "No VM detected", 0xd, 0xa, 0 
+	falseMsg db "No VM detected", 0xd, 0xa, 0 
 	; if in VM, print "1"
-	yesVm db "VM detected", 0xd, 0xa, 0
+	trueMsg db "VM detected", 0xd, 0xa, 0
 
 segment .text
 global main
-extern ExitProcess
-
-extern printf
-
 main:
 	push rbp
 	mov rbp, rsp
-	sub rsp, 32
-
 	xor eax, eax
 	inc eax ; eax=1 provides processor features and model related info.
 	CPUID	;*CPUID* provides processor details
 	bt ecx, 0x1f	;feature flag 0x1f(31) = 'CPUID_FEAT_ECX_HYPERVISOR' if true, then VM (yes)
-	jne Vmtrue
+	je yesVm
 
-	lea rcx, [noVm]
+	; this code gets executed if it's not inside a virtual machine (VM)
+	lea ecx, [falseMsg]
 	call printf
 	jmp end
 
-	Vmtrue:
-		lea rcx, [yesVm]
+	yesVm:
+		lea ecx, [trueMsg]
 		call printf
 		jmp end
 
 	end:
-	xor eax, eax
-	call ExitProcess
+		xor eax, eax
+		call ExitProcess
