309aa85bb7e9eb1a2e80fda0979d328915dca3e0
Dan <daniel.d.cummings@gmail.com>
2024-05-25 19:24:01 -0700

    Update novm.asm

--- novm.asm
+++ novm.asm
@@ -18,7 +18,7 @@ main:
 	inc eax ; eax=1 provides processor features and model related info.
 	CPUID	;*CPUID* provides processor details
 	bt ecx, 0x1f	;feature flag 0x1f(31) = 'CPUID_FEAT_ECX_HYPERVISOR' if true, then VM (yes)
-	jnc noVm ; cf = 1 if VM is detected
+	jnc noVm 
 
 	; this code gets executed if it's not inside a virtual machine (VM)
 	lea rcx, [trueMsg]
